3122640f39319b37a73db774bca7c728eaf75478
BennyThink <benny.think@gmail.com>
2021-11-21 22:48:28 +0800

    complete image for ubuntu, centos and debian

--- fabfile.py
+++ fabfile.py
@@ -9,36 +9,22 @@ registry = "ghcr.io/bennythink"
 # fabric3
 def base():
     local(f"docker build -f images/base/Dockerfile -t {registry}/base .")
-
-
-def ubuntu(version="20.04"):
-    local(f"docker build -f images/ubuntu/{version}/Dockerfile -t {registry}/ubuntu:{version} .")
+    local(f"docker push {registry}/base ")
 
 
 def all():
     for filename in glob.iglob("images/**/Dockerfile", recursive=True):
-        each: "list" = filename.split(os.sep)
-        if len(each) == 4:
-            version = each[-2]
-        else:
-            version = "latest"
-        local(f"docker build -f {filename} -t {registry}/{each[1]}:{version} .")
-
-
-def push():
-    for filename in glob.iglob("images/**/Dockerfile", recursive=True):
-        each: "list" = filename.split(os.sep)
-        if len(each) == 4:
-            version = each[-2]
-        else:
-            version = "latest"
-        local(f"docker push  {registry}/{each[1]}:{version} ")
+        dirs: "list" = filename.split(os.sep)
+        if "base" not in dirs:
+            version = dirs[-2]
+            distro = dirs[-3]
+            local(f"docker build -f {filename} --build-arg version={version} -t {registry}/{distro}:{version} .")
+            local(f"docker push {registry}/{distro}:{version}")
 
 
 def start():
     base()
     all()
-    push()
 
 
 def run(image=f"{registry}/ubuntu:20.04", port=2222, rm=True):
